9310a7eab980994fb2790addc76892483379a36f
PJY00 <kdio@chungbuk.ac.kr>
2024-12-17 18:12:37 +0900

    FIX BG save load color
    색상 변경

--- BG_saveload.py
+++ BG_saveload.py
@@ -16,36 +16,39 @@ for event in pygame.event.get():
 
 pygame.display.set_caption("Escape School")
 
-WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
 
 font_path = 'neodgm_code.ttf'  # 폰트 파일 경로
 font_middle = pygame.font.Font(font_path, 48)  # 폰트 크기 48로 설정
 font_small = pygame.font.Font(font_path, 30)  # 폰트 크기 48로 설정
 
 # 텍스트 렌더링
-title_name = font_middle.render("불러오기", True, (255, 0, 0))  # 빨간색
-F4_load = font_small.render("4층", True, (0, 255, 0))  # 초록색
-F3_load = font_small.render("3층", True, (0, 0, 255))  # 파란색
-F2_load = font_small.render("2층", True, (255, 255, 0))  # 노란색
-F1_load = font_small.render("1층", True, (255, 255, 0))  # 노란색
+title_name = font_middle.render("불러오기", True, (255, 255, 255))  # 빨간색
+F4_load = font_small.render("4층", True, (255, 255, 255))  # 초록색
+F3_load = font_small.render("3층", True, (255, 255, 255))  # 파란색
+F2_load = font_small.render("2층", True, (255, 255, 255))  # 노란색
+F1_load = font_small.render("1층", True, (255, 255, 255))  # 노란색
 
 # 텍스트 위치 설정
-title_name_rect = title_name.get_rect(center=(WIDTH // 2, HEIGHT // 5))  # 상단 중앙
-F4_load_rect = F4_load.get_rect(center=(WIDTH // 2, HEIGHT * 6 // 12))  # 화면 중앙
-F3_load_rect = F3_load.get_rect(center=(WIDTH // 2, HEIGHT * 7 // 12))  # 하단 3/4 지점
-F2_load_rect = F2_load.get_rect(center=(WIDTH // 2, HEIGHT * 8 // 12))  # 하단 4/5 지점
-F1_load_rect = F1_load.get_rect(center=(WIDTH // 2, HEIGHT * 10/12))
+title_name_rect = title_name.get_rect(center=(WIDTH // 2, HEIGHT * 1 // 5))  # 상단 중앙
+F4_load_rect = F4_load.get_rect(center=(WIDTH // 2, HEIGHT * 4 // 10))  # 화면 중앙
+F3_load_rect = F3_load.get_rect(center=(WIDTH // 2, HEIGHT * 5 // 10))  # 하단 3/4 지점
+F2_load_rect = F2_load.get_rect(center=(WIDTH // 2, HEIGHT * 6 // 10))  # 하단 4/5 지점
+F1_load_rect = F1_load.get_rect(center=(WIDTH // 2, HEIGHT * 7 // 10))
 
 # 클릭 이벤트 함수
 def handle_click(pos):
     if F4_load_rect.collidepoint(pos):
         F4_main()
     elif F3_load_rect.collidepoint(pos):
-        F3_main()
+        #F3_main()
+        print("1")
     elif F2_load_rect.collidepoint(pos):
-        F2_main()
+        #F2_main()
+        print("1")
     elif F1_load_rect.collidepoint(pos):
-        F1_main()
+        #F1_main()
+        print("1")
 
 # 화면 업데이트 루프
 running = True
@@ -62,7 +65,7 @@ while running:
             WIDTH, HEIGHT = event.w, event.h
             screen = pygame.display.set_mode((WIDTH, HEIGHT), pygame.RESIZABLE)
     
-    screen.fill(WHITE)
+    screen.fill(BLACK)
 
      # 텍스트 화면에 그리기
     screen.blit(title_name, title_name_rect)
